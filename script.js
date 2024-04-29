15cfd213ee9260d1de373fd6f61b4dcb49235ba9
Mica-zonta <micaelaineszonta@gmail.com>
2024-04-29 15:42:39 -0300

    avances

--- script.js
+++ script.js
@@ -20,8 +20,8 @@ const reproducir=()=>{
 /*juguemos*/
 
 
-let imagen1=document.querySelector('#img1');
-let destino=document.querySelectorAll('#tarjeta');
+/*let imagen1=document.querySelector('#img1');
+let destino=document.querySelector('#tarjeta');
 
 imagen1.addEventListener('dragstart', inicioTraslado);
 imagen1.addEventListener('dragend', finalTraslado);
@@ -47,10 +47,33 @@ function soltarElemento(evento){
 
 function prevenirDefault(evento){
     evento.prevenirDefault()
+};*/
+
+
+let imagen1 = document.querySelector('#img1');
+let destino = document.querySelector('#tarjeta'); // Cambiado a querySelector
+
+imagen1.addEventListener('dragstart', inicioTraslado);
+imagen1.addEventListener('dragend', finalTraslado);
+
+function inicioTraslado(evento){
+    evento.dataTransfer.setData('text/plain', "./assets/imagenes/Rompe1.png"); // Cambiado 'Text' a 'text/plain'
 };
 
+function finalTraslado(evento){
+    let imagen1 = evento.target;
+    imagen1.style.visibility = 'hidden';
+};
 
+destino.addEventListener('dragover', prevenirDefault);
+destino.addEventListener('drop', soltarElemento);
 
+function soltarElemento(evento){
+    evento.preventDefault(); // Cambiado a preventDefault() y corregido el cierre de comillas en id='img1'
+    let dataImagen1 = evento.dataTransfer.getData('text/plain'); // Cambiado 'Text' a 'text/plain'
+    destino.innerHTML = `<img id='img1' src="${dataImagen1}" />`; // Corregido cierre de comillas
+};
 
-
-
+function prevenirDefault(evento){
+    evento.preventDefault();
+};
